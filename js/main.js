3324cffdda6198f2f4d76f9d1c886e03bcbb9a2d
motomotoyoshi <motomoto.git@gmail.com>
2020-01-27 03:19:16 +0900

    Update, show messages

--- js/main.js
+++ js/main.js
@@ -8,12 +8,21 @@ const collection = db.collection('messages');
 
 const message = document.getElementById('message');
 const form = document.querySelector('form');
+const messages = document.getElementById('messages');
+
+collection.get().then(snapshot => {
+  snapshot.forEach(doc => {
+    const li = document.createElement('li');
+    li.textContent = doc.data().message;
+    messages.appendChild(li);
+  });
+});
 
 form.addEventListener('submit', e => {
   e.preventDefault();
 
   collection.add({
-    messages: message.value,
+    message: message.value,
   })
     .then(doc => {
       console.log(`${doc.id} added.`);
