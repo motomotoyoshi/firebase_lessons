c162a10031822b833c855fdc9b45e24c33568cb5
motomotoyoshi <motomoto.git@gmail.com>
2020-01-27 02:54:58 +0900

    Update, add form

--- js/main.js
+++ js/main.js
@@ -6,12 +6,23 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 const collection = db.collection('messages');
 
-collection.add({
-  messages: 'test',
-})
-.then(doc => {
-  console.log(`${doc.id} added.`);
-})
-.catch(error => {
-  console.log(error);
+const message = document.getElementById('message');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  collection.add({
+    messages: message.value,
+  })
+    .then(doc => {
+      console.log(`${doc.id} added.`);
+      message.value = '';
+      message.focus();
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
+
+message.focus();
