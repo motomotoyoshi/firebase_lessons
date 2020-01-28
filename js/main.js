d1222328a3139540c42054718e2649cfc73dd850
motomotoyoshi <motomoto.git@gmail.com>
2020-01-29 01:51:24 +0900

    Update

--- js/main.js
+++ js/main.js
@@ -1,76 +1,82 @@
-'use strict'
-
 import { firebaseConfig } from '../conf.js';
-firebase.initializeApp(firebaseConfig);
+(() => {
+  'use strict'
 
-const db = firebase.firestore();
-const collection = db.collection('messages');
-const auth = firebase.auth();
-let me = null;
+  firebase.initializeApp(firebaseConfig);
 
-const message = document.getElementById('message');
-const form = document.querySelector('form');
-const messages = document.getElementById('messages');
-const login = document.getElementById('login');
-const logout = document.getElementById('logout');
+  const db = firebase.firestore();
+  const collection = db.collection('messages');
+  const auth = firebase.auth();
+  let me = null;
 
-login.addEventListener('click', () => {
-  auth.signInAnonymously();
-});
-logout.addEventListener('click', () => {
-  auth.signOut();
-});
+  const message = document.getElementById('message');
+  const form = document.querySelector('form');
+  const messages = document.getElementById('messages');
+  const login = document.getElementById('login');
+  const logout = document.getElementById('logout');
 
-auth.onAuthStateChanged(user => {
-  if (user) {
-    me = user;
-    collection.orderBy('created').onSnapshot(snapshot => {
-      snapshot.docChanges().forEach(change => {
-        if (change.type === 'added') {
-          const li = document.createElement('li');
-          const d = change.doc.data();
-          li.textContent = d.uid.substr(0, 8) + ': ' + d.message;
-          messages.appendChild(li);
-        }
-      });
-    });
-    console.log(`Logged in as: ${user.uid}`);
-    login.classList.add('hidden');
-    [logout, form, messages].forEach(el => {
-      el.classList.remove('hidden');
-    });
-    message.focus();
-    return;
-  }
-  me = null;
-  console.log('Nobody is logged in.');
-  login.classList.remove('hidden');
-  [logout, form, messages].forEach(el => {
-    el.classList.add('hidden');
+  login.addEventListener('click', () => {
+    auth.signInAnonymously();
   });
-});
+  logout.addEventListener('click', () => {
+    auth.signOut();
+  });
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+  auth.onAuthStateChanged(user => {
+    if (user) {
+      me = user;
 
-  const val = message.value.trim();
-  if (val === "") {
-    return;
-  }
-
-  message.value = '';
-  message.focus();
-
-  collection.add({
-    message: val,
-    created: firebase.firestore.FieldValue.serverTimestamp(),
-    uid: me ? me.uid : 'nobody',
-  })
-    .then(doc => {
-      console.log(`${doc.id} added.`);
-    })
-    .catch(error => {
-      console.log(error);
+      while(messages.firstChild) {
+        messages.removeChild(messages.firstChild);
+      };
+      
+      collection.orderBy('created').onSnapshot(snapshot => {
+        snapshot.docChanges().forEach(change => {
+          if (change.type === 'added') {
+            const li = document.createElement('li');
+            const d = change.doc.data();
+            li.textContent = d.uid.substr(0, 8) + ': ' + d.message;
+            messages.appendChild(li);
+          }
+        });
+      }, error => {});
+      console.log(`Logged in as: ${user.uid}`);
+      login.classList.add('hidden');
+      [logout, form, messages].forEach(el => {
+        el.classList.remove('hidden');
+      });
+      message.focus();
+      return;
+    }
+    me = null;
+    console.log('Nobody is logged in.');
+    login.classList.remove('hidden');
+    [logout, form, messages].forEach(el => {
+      el.classList.add('hidden');
     });
-});
+  });
 
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    const val = message.value.trim();
+    if (val === "") {
+      return;
+    }
+
+    message.value = '';
+    message.focus();
+
+    collection.add({
+      message: val,
+      created: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: me ? me.uid : 'nobody',
+    })
+      .then(doc => {
+        console.log(`${doc.id} added.`);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+})();
